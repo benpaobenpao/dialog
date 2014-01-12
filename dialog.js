f7a6aefb5b7ba26dc224d11252ccc426675e8901
benpaobenpao <benpaobenpao@126.com>
2014-01-12 16:55:24 +0800

    dialog

---
+++ dialog.js
@@ -0,0 +1,86 @@
+/* https://github.com/benpaobenpao/dialog */
+(function($) {
+    $.Dialog = {
+        init: function(options) {
+            var _this = this,
+                defaults = {
+                    width: "400",
+                    height: "100",
+                    title: "",
+                    html: "",
+                    type: "default", //  default   conform   alert
+                    closed: null,
+                    conform: {
+                        name: "确定",
+                        callback: null
+                    },
+                    cancel: {
+                        name: "取消",
+                        callback: null
+                    }
+                };
+            options.isIE6 = !! window.ActiveXObject && !window.XMLHttpRequest;
+            _this.options = $.extend({}, defaults, options);
+            _this.creatHtml();
+            _this.show();
+            _this.events();
+            if (_this.options.isIE6) {
+                _this.ie6fixed();
+            }
+        },
+        creatHtml: function() {
+            var _this = this;
+            var interText = doT.template($("#dialogtmpl")[0].text);
+            $("body").append(interText(_this.options));
+        },
+        show: function() {
+            var _this = this;
+            _this.dialogbg = $("#dialogbg");
+            _this.dialogbox = $("#dialogbox");
+
+            _this.dialogbg.css({
+                "height": $(document).height()
+            });
+
+            _this.dialogbox.css({
+                "margin-top": (-_this.options.height / 2) + "px",
+                "margin-left": (-_this.options.width / 2) + "px",
+                "width": _this.options.width + "px",
+                "height": _this.options.height + "px"
+            });
+
+            _this.dialogbox.addClass("dialogboxrunstart");
+        },
+        events: function() {
+            var _this = this;
+            _this.dialogclosed = _this.dialogbox.find(".dialogclosed");
+            _this.dialogconform = _this.dialogbox.find(".dialogconform");
+            _this.dialogcancel = _this.dialogbox.find(".dialogcancel");
+
+            _this.dialogclosed.on("click", function() {
+                _this.removeCallback(_this.options.closed);
+            });
+
+            _this.dialogconform.on("click", function() {
+                _this.removeCallback(_this.options.conform.callback);
+            });
+
+            _this.dialogcancel.on("click", function() {
+                _this.removeCallback(_this.options.cancel.callback);
+            });
+        },
+        removeCallback: function(call) {
+            var _this = this;
+            _this.dialogbg.remove();
+            _this.dialogbox.remove(); !! call && call();
+        },
+        ie6fixed: function() {
+            var _this = this;
+            $(window).scroll(function() {
+                _this.dialogbox.css({
+                    "top": (($(window).height() - _this.options.height) / 2 + $(document).scrollTop()) + "px"
+                });
+            });
+        }
+    }
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,45 @@
+基于jquery和dot.js弹出框插件，兼容IE6+等其他浏览器。
+
+参数使用说明：
+width：定义弹出框的宽度，默认值是400。
+height：定义弹出框的高度，默认值是100.
+title：定义弹出框的标题，默认值是空。
+html：定义弹出框的内容，默认值是空。
+type：定义弹出框的类型，默认值是default，其他conform和alert。
+closed: 标题栏中的关闭按钮，回调方法，默认为null。
+conform：包含name指按钮的名称，默认值为确定，callback回调方法，默认值为null。
+cancel：包含name指按钮的名称，默认值为取消，callback回调方法，默认值为null。
+备注：如果使用conform或alert时，不设置type类型是不起作用。
+
+放在公用页面底部
+<script id="dialogtmpl" type="text/x-dot-template">
+    {{? it.isIE6===false }} 
+    <div id="dialogbg" class="dialogbg"></div>
+    {{??  }}
+    <iframe id="dialogbg" class="dialogbg"></iframe>
+    {{?}}
+    <div id="dialogbox" class="dialogbox">
+        <div class="dialogcont">
+            <div class="dialogtit clearfix">
+                <a class="dialogclosed" title="关闭">&otimes;</a>
+                <div class="dialogtxt">{{=it.title}}</div>
+            </div>
+            <div class="dialogmain">{{=it.html}}</div>
+            {{? it.type === "conform"}}
+            <div class="dialogBtns"><a class="dialogconform">{{=it.conform.name||"确定"}}</a></div>
+            {{?? it.type === "alert"}}
+            <div class="dialogBtns"><a class="dialogconform">{{=it.conform.name||"确定"}}</a><a class="dialogcancel">{{=it.cancel.name||"取消"}}</a></div>
+            {{?}} 
+        </div>
+    </div>
+</script>
+
+例如：
+$.Dialog.init({
+    height: 180,
+    title: "弹出框",
+    html: "亲，默认弹出框哦",
+    closed: function() {
+        alert("关闭");
+    }
+});
